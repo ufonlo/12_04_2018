30c19511e6f89036fc8df9bb49134f2a26fa1524
Henadii Bohovuk <socufo@gmail.com>
2018-04-21 20:36:56 +0300

    next page

--- src/Lesons_21_04_2018/Main.java
+++ src/Lesons_21_04_2018/Main.java
@@ -1,7 +1,11 @@
 package Lesons_21_04_2018;
 
 public class Main {
+    static  Cat cat ;
+    //static People people = new People();
+
     public static void main(String[] args) {
 
+        cat = new Cat(2,"pers","Tom");
     }
 }
